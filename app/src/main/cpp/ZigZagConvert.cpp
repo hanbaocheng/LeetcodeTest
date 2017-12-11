834be579f7854097d9990bfcb79279d1d3ead0e7
Baocheng Han <hanbaocheng19@gmail.com>
2017-12-11 22:59:29 +0100

    ZigZag convert finish

---
+++ app/src/main/cpp/ZigZagConvert.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Baocheng Han on 12/11/17.
+//
+#include "Solution.h"
+
+static const auto ____ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie();
+    return nullptr;
+}();
+
+string Solution::zigzagconvert(string s, int numRows) {
+    string zigzag;
+    int unit = numRows + numRows - 2;
+    if (numRows == 1) unit = 1;
+
+    for (int i = 0; i < numRows; ++i) {
+        int index = i;
+        while (index < s.size()) {
+            zigzag.push_back(s[index]);
+
+            if(i > 0 && i < numRows - 1) {
+                int zigzag_index = index / unit * unit + (unit - index % unit);
+                if(zigzag_index < s.size())
+                    zigzag.push_back(s[zigzag_index]);
+            }
+
+            index += unit;
+        }
+    }
+    return zigzag;
+}
