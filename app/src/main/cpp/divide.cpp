2814a844635b33a6fa1e6b5cf533a3ed41a3e6c5
Baocheng Han <baocheng.han@medion.com>
2017-12-28 17:05:39 +0100

    findSubstring

--- app/src/main/cpp/divide.cpp
+++ app/src/main/cpp/divide.cpp
@@ -4,52 +4,110 @@
 #include "Solution.h"
 
 int Solution::divide(int dividend, int divisor) {
-    if (dividend == INT32_MIN && divisor == -1) {
+    int mydividend = dividend;
+    int mydivisor = divisor;
+
+    if (mydividend == INT32_MIN && mydivisor == -1) {
         return INT32_MAX;
     }
 
-    if (dividend == INT32_MIN && divisor == 1) {
+    if (mydividend == INT32_MIN && mydivisor == 1) {
         return INT32_MIN;
     }
 
-    int ret = 0; bool flag = (dividend & 0x80000000) ^ (divisor & 0x80000000);
-    if (divisor < 0)
+    int ret = 0;
+    bool sign = (mydividend & 0x80000000) ^ (mydivisor & 0x80000000);
+    if (mydivisor < 0)
     {
-        if (divisor == INT32_MIN) {
-            if (dividend == INT32_MIN)
+        if (mydivisor == INT32_MIN) {
+            if (mydividend == INT32_MIN)
                 return 1;
             else return 0;
         }
-        divisor *= -1;
+        mydivisor = -1 * mydivisor;
     }
 
 
-    if (dividend < 0) {
-        if (dividend == INT32_MIN)
+    if (mydividend < 0) {
+        if (mydividend == INT32_MIN)
         {
-            dividend += divisor;
+            mydividend += mydivisor;
             ret++;
         }
-        dividend *= -1;
+        mydividend *= -1;
+    }
+
+
+    while (mydividend >= mydivisor) {
+
+/*        for (int i = 0; (mydivisor << i) > 0 && mydividend >= (mydivisor << i); ++i) {
+            ret += (1 << i);
+            mydividend -= (mydivisor << i);
+        }*/
+
+        int quotient = -1;
+        for (int i = 0; (mydivisor << i) > 0 && mydividend >= (mydivisor << i); ++i) {
+            quotient = i;
+        }
+
+        if (quotient > -1) {
+            ret += (1 << quotient);
+            mydividend -= (mydivisor << quotient);
+        }
+    }
+
+    return sign == 0 ? ret : -1 * ret;
+}
+int divide1(int dividend, int divisor) {
+    int mydividend = dividend;
+    int mydivisor = divisor;
+
+    if (mydividend == INT32_MIN && mydivisor == -1) {
+        return INT32_MAX;
+    }
+
+    if (mydividend == INT32_MIN && mydivisor == 1) {
+        return INT32_MIN;
+    }
+
+    int ret = 0; bool sign = (mydividend & 0x80000000) ^ (mydivisor & 0x80000000);
+    if (mydivisor < 0)
+    {
+        if (mydivisor == INT32_MIN) {
+            if (mydividend == INT32_MIN)
+                return 1;
+            else return 0;
+        }
+        mydivisor = -1 * mydivisor;
+    }
+
+
+    if (mydividend < 0) {
+        if (mydividend == INT32_MIN)
+        {
+            mydividend += mydivisor;
+            ret++;
+        }
+        mydividend *= -1;
     }
 
     int factor = 1;
-    int aDivisior = divisor * factor;
-    while (dividend > aDivisior) {
-        dividend -= aDivisior;
+    int aDivisior = mydivisor * factor;
+    while (aDivisior >= mydivisor && mydividend >= aDivisior) {
+        mydividend -= aDivisior;
         ret += factor;
         factor++;
-        aDivisior = divisor * factor;
+        aDivisior = mydivisor * factor;
     }
 
     do{
         factor--;
-        aDivisior = divisor * factor;
-        if(dividend >=aDivisior) {
-            dividend -= aDivisior;
+        aDivisior = mydivisor * factor;
+        if(mydividend >=aDivisior) {
+            mydividend -= aDivisior;
             ret += factor;
         }
-    } while (dividend > divisor);
+    } while (mydividend > mydivisor);
 
-    return flag == 0 ? ret : -1 * ret;
+    return sign == 0 ? ret : -1 * ret;
 }
--- app/src/main/cpp/strStr.cpp
+++ app/src/main/cpp/strStr.cpp
@@ -37,3 +37,76 @@ int Solution::strStr(string haystack, string needle) {
 
     return j == pSize ? i - j : -1;
 }
+
+void dp(map<int, bool> &memo, int start, string s, vector<string>& words, vector<int>& ret) {
+    if (memo.find(start) != memo.end()) {
+        return;
+    }
+
+    int wordlen = words[0].size();
+    int sublen = wordlen * words.size();
+
+    if (start > s.size() - sublen)
+        return;
+
+    map<int, int> index_to_string;
+    for (int i = 0; i < words.size(); ++i) {
+        int index = s.find(words[i], start);
+        if (index == s.npos) {
+            return;
+        }
+
+        while (index_to_string.find(index) != index_to_string.end()) {
+            index = s.find(words[i], index + wordlen);
+            if (index == s.npos) {
+                return;
+            }
+        }
+        index_to_string[index] = i;
+    }
+
+    while (index_to_string.size() == words.size()) {
+        int first = index_to_string.begin()->first;
+        int last = index_to_string.rbegin()->first;
+        int next = last + wordlen;
+        if (next - first == sublen) {
+            memo[first] = true;
+            ret.push_back(first);
+            int size = memo.size();
+
+            int firstword = index_to_string[first];
+            if (s.find(words[firstword], next) == next) {
+                index_to_string[next] = firstword;
+                index_to_string.erase(first);
+            } else {
+                dp(memo, next, s, words, ret);
+                break;
+            }
+            dp(memo, first + 1, s, words, ret);
+        } else {
+            memo[first] = false;
+            int firstword = index_to_string[first];
+            int pos = s.find(words[firstword], first + wordlen);
+            while (index_to_string.find(pos) != index_to_string.end()) {
+                pos = s.find(words[firstword], pos + wordlen);
+            }
+
+            if (pos != s.npos) {
+                index_to_string[pos] = firstword;
+                index_to_string.erase(first);
+            } else {
+                return;
+            }
+        }
+    }
+    return;
+}
+
+vector<int> Solution::findSubstring(string s, vector<string>& words) {
+    vector<int> ret;
+    map<int, bool> memo;
+    dp(memo, 0, s, words, ret);
+    sort( ret.begin(), ret.end() );
+    ret.erase( unique( ret.begin(), ret.end() ), ret.end() );
+    return ret;
+}
